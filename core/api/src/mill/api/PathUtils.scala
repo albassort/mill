06dd1122d13c5866f118837fb3fd17d89932afb9
user <carilinemarceano@albassort.com>
2025-03-30 15:17:34 -0400

    Paths serialization start, with prototype

---
+++ core/api/src/mill/api/PathUtils.scala
@@ -0,0 +1,37 @@
+package mill.api
+import os.Path
+import upickle.default.ReadWriter as RW
+import scala.reflect.ClassTag
+import scala.util.matching.Regex
+import mill.api.WorkspaceRoot
+/**
+ * Defines a trait which handles deerialization of paths, in a way that can be used by both path refs and paths
+ */
+trait PathUtils {
+
+  implicit def substitutions() : List[(String, String)] = 
+    val workspaceRootPath : String = WorkspaceRoot.workspaceRoot.toString
+    var result = List((workspaceRootPath, "*$WorkplaceRoot*"))
+
+    result
+
+  
+  implicit def serializeEnvVariables(a : os.Path) : String = 
+    // TODO: Make parsing this a little bit more complex. The 
+    val subs = substitutions()
+    var result = a.toString
+    subs.foreach{ case (value,sub) => 
+      //Serializes by replacing the path with the substitution
+      result = result.replace(value, sub)
+    }
+    result
+  
+
+  implicit def deserializeEnvVariables(a : String) : os.Path = 
+    val subs = substitutions()
+    var result = a
+    subs.foreach{ case (value,sub) => 
+      result = result.replace(sub, value)
+    }
+    os.Path(result)
+}
