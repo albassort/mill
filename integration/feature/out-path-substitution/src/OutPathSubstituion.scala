5c4bbc6b5c265f42362decbbc03774b1fc6d00e3
user <carilinemarceano@albassort.com>
2025-04-05 04:44:03 -0400

    Added a comment for clarification

--- integration/feature/out-path-substitution/src/OutPathSubstituion.scala
+++ integration/feature/out-path-substitution/src/OutPathSubstituion.scala
@@ -24,7 +24,7 @@ object OutPathTestSuite extends UtestIntegrationTestSuite {
         ) {
 
           if (stringified.contains("ref:")) {
-
+            //removes the prefix of a ref: string, which contains noise and would throw off our readings
             result += (s"$path.$k" -> stringified.substring(16))
           } else {
             result += (s"$path.$k" -> stringified)
@@ -128,7 +128,6 @@ object OutPathTestSuite extends UtestIntegrationTestSuite {
       reference.foreach { case (k, v) =>
         assert(modified.contains(k))
       }
-
     }
   }
 }
